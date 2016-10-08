3dc8c1686016dc3aeee070dbbf999bda741d2c19
Mark Stephenson <mark.s@evvnt.com>
2016-10-08 15:18:25 +0100

    - Updated events view to move scss to scss sheet rather than inline - Updated carrierwave settings so works local and dev

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,11 +7,10 @@ CarrierWave.configure do |config|
     region:                 'eu-west-1'
   }
 
-  # For testing, upload files to local `tmp` folder.
   if Rails.env.test? || Rails.env.cucumber? || Rails.env.development?
-    config.storage              = :file
-    config.enable_processing    = false
-    config.root                 = "#{Rails.root}/tmp" unless Rails.env.development?
+    config.storage           = :file
+    config.enable_processing = false
+    config.root              = "#{Rails.root}/tmp" unless Rails.env.development?
   else
     config.storage = :fog
   end
@@ -21,6 +20,4 @@ CarrierWave.configure do |config|
 
   config.fog_directory    = ENV['AWS_BUCKET_NAME']
   config.fog_public       = true
-  # config.s3_access_policy = :public_read
-  # config.fog_host         = "#{ENV['AWS_ASSET_URL']}/#{ENV['AWS_BUCKET_NAME']}"
 end
